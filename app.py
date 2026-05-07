1369427ddfa68fc5cc90f8aaaa22b4eddf0eed46
apipiunai <ikucastro23@lhusurbil.eus>
2026-05-07 16:15:48 +0200

    07-05-2026

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, jsonify, request, send_file, after_this_request, render_template
+from flask import Flask, jsonify, request, os
 from flask_cors import CORS 
-import os
 from supabase import create_client, Client
 from werkzeug.security import generate_password_hash, check_password_hash
 
